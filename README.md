f7bfbcb77156d061c84a5042130a48823a525103
Italo Maia <italo.maia@gmail.com>
2018-11-11 21:24:34 +0100

    Adding readme

---
+++ README.md
@@ -0,0 +1,13 @@
+Docker-Resty-Lapis
+==================
+
+Uses official openresty image to create a alpine+lapis container.
+
+Getting started
+===============
+
+```
+git clone git@github.com:italomaia/docker-resty-lapis.git
+cd docker-resty-lapis
+./build.sh
+```
--- build.sh
+++ build.sh
@@ -3,5 +3,14 @@ RESTY_IMAGE="$(whoami)/openresty"
 RESTY_IMAGE_VERSION="fat"
 RESTY_LUAROCKS_VERSION="3.0.4"
 
-docker build -t $RESTY_IMAGE:$RESTY_IMAGE_VERSION --build-arg RESTY_LUAROCKS_VERSION="$RESTY_LUAROCKS_VERSION" -f Dockerfile.fat git@github.com:openresty/docker-openresty.git#master:alpine
-docker build -t $(whoami)/lapis:alpine --build-arg RESTY_IMAGE="$RESTY_IMAGE" --build-arg RESTY_IMAGE_VERSION="$RESTY_IMAGE_VERSION" .
+docker build -t $RESTY_IMAGE:$RESTY_IMAGE_VERSION \
+    --build-arg RESTY_LUAROCKS_VERSION="$RESTY_LUAROCKS_VERSION" \
+    -f Dockerfile.fat \
+    git@github.com:openresty/docker-openresty.git#master:alpine
+
+docker build -t $(whoami)/lapis:alpine \
+    --build-arg RESTY_IMAGE="$RESTY_IMAGE" \
+    --build-arg RESTY_IMAGE_VERSION="$RESTY_IMAGE_VERSION" \
+    .
+
+echo "Image \"$(whoami)/lapis:alpine\" created."
